c34f4fa811a90cd914406fc7e238f95652832429
+ <+>
2024-09-19 14:39:25 +0300

    created functions that will help the user to interact with the command line

---
+++ lib/helpers.py
@@ -0,0 +1,54 @@
+from models.author import Author
+from models.book import Book
+
+def exit_program():
+    print("Thank you for visiting Come again 👋")
+    exit()
+    
+#author functions to be implemented
+    
+def list_authors():
+    pass
+
+def find_author_by_id():
+    pass
+
+def find_author_by_name():
+    pass
+
+def find_author_by_genre():
+    pass
+
+def create_new_author():
+    pass
+
+def update_author():
+    pass
+
+def delete_author():
+    pass
+
+#book functions to be implemented
+
+def list_books():
+    pass
+
+def find_book_by_id():
+    pass
+
+def find_book_by_title():
+    pass
+
+def find_book_by_year_published():
+    pass
+        
+def create_new_book():
+    pass
+
+def update_book():
+    pass
+
+def delete_book():
+    pass
+
+      
